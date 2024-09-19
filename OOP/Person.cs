ab1e78de804f0e64aae5ac677d0f626fc26bb395
Luna L <hanken95@gmail.com>
2024-09-19 15:19:12 +0200

    Add project files.

---
+++ OOP/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Person
+    {
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+		private string fName;
+
+		public string FName
+		{
+			get { return fName; }
+			set { fName = value; }
+		}
+		private string lName;
+
+		public string LName
+		{
+			get { return lName; }
+			set { lName = value; }
+		}
+		private int height;
+
+		public int Height
+		{
+			get { return height; }
+			set { height = value; }
+		}
+		private int weight;
+
+		public int Weight
+		{
+			get { return weight; }
+			set { weight = value; }
+		}
+
+	}
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            //person.age = 3; går inte. Kommer inte åt fältet direkt
+            //Men detta funkar
+            person.Age = 3;
+
+        }
+    }
+}
